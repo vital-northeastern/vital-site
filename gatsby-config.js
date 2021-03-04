799d6c53ab1f70fa4b5cd31d133ba382adad417c
sreya-kata <sreya.kata@gmail.com>
2021-03-03 21:29:39 -0500

    updated gatsby-config and license

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
