1763fb530f8bf843ba6c571a57cfeb142377091b
sreya-kata <sreya.kata@gmail.com>
2021-03-17 17:58:51 -0400

    added api data for team page

--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -1,16 +1,57 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
+import get from "lodash/get"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const TeamPage = () => (
-  <Layout>
-    <SEO title="Team Page" />
-    <h1>Our Team</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const TeamPage = props => {
+  const teamPage = get(props, "data.contentfulTeamPage")
+
+  return (
+    <Layout>
+      <SEO title="Team Page" />
+      <h1>{teamPage.title}</h1>
+      <ul className="members">
+        {teamPage.teamMembers.map((member, index) => {
+          return (
+            <li className="member">
+              <h2>{member.name}</h2>
+              <p className="excerpt">{member.email}</p>
+              <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Img
+                  className="featured"
+                  fluid={member.photo.fluid}
+                  alt={member.name}
+                />
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
 
 export default TeamPage
+
+export const pageQuery = graphql`
+  query teamQuery {
+    contentfulTeamPage {
+      title
+      teamMembers {
+        name
+        photo {
+          fluid(maxWidth: 750) {
+            ...GatsbyContentfulFluid
+          }
+        }
+        email
+        major
+        position
+        linkedinUrl
+        team
+      }
+    }
+  }
+`
