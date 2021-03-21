7066f02afc02b9484e93c2f3fce6e31a609232cc
sreya-kata <sreya.kata@gmail.com>
2021-03-21 11:14:39 -0400

    implicit <div> tags -for time-being-

--- src/components/what-we-do/industry-bg.js
+++ src/components/what-we-do/industry-bg.js
@@ -3,11 +3,11 @@ import Img from "gatsby-image"
 
 const IndustryBG = ({ title, blurb, image, imageAlt }) => {
   return (
-    <div>
+    <>
       <h3>{title}</h3>
       <p>{blurb.industryBackgroundBlurb}</p>
       <Img className="featured" fluid={image.fluid} alt={imageAlt} />
-    </div>
+    </>
   )
 }
 
--- src/components/what-we-do/programming-card.js
+++ src/components/what-we-do/programming-card.js
@@ -3,11 +3,11 @@ import Img from "gatsby-image"
 
 const ProgrammingCard = ({ title, description, photo, photoAlt }) => {
   return (
-    <div>
+    <>
       <h4>{title}</h4>
       <p>{description.shortDescription}</p>
       <Img className="featured" fluid={photo.fluid} alt={photoAlt} />
-    </div>
+    </>
   )
 }
 
