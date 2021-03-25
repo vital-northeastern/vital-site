b7d318a110245dad5c443eaa2e530af165ecdfb5
sreya-kata <sreya.kata@gmail.com>
2021-03-25 19:54:26 -0400

    added all initial color values

---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+export const white = "#FFFFFF"
+export const black = "#000000"
+export const darkGray = "#3D3939"
+export const lightGray = "#F3F3F3"
+export const primaryBlue = "#5269E1"
+export const secondayBlue = "#546BDF"
+export const darkBlue = "#001049"
