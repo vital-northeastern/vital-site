903a841a38d1f7f368df05fe35341b26c51ecd32
Ryanjso <ryanjsoderberg@gmail.com>
2021-04-02 12:51:27 -0400

    added gcal iframe

---
+++ src/components/whats-happening/calendar-styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+import { devices } from "../../constants/devices"
+
+const CalendarSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const CalendarFrameLarge = styled.iframe`
+  border: none;
+  max-width: 775px;
+  display: none;
+  @media ${devices.tablet} {
+    display: block;
+  }
+`
+
+const CalendarFrameSmall = styled.iframe`
+  border: none;
+  display: block;
+  width: 90%;
+  @media ${devices.tablet} {
+    display: none;
+  }
+`
+
+export { CalendarSection, CalendarFrameLarge, CalendarFrameSmall }
--- src/components/whats-happening/calendar.js
+++ src/components/whats-happening/calendar.js
@@ -1,10 +1,28 @@
 import React from "react"
+import {
+  CalendarSection,
+  CalendarFrameLarge,
+  CalendarFrameSmall,
+} from "./calendar-styles"
+import { HLower } from "../../constants/typography"
 
 const Calendar = ({ title, embedCode }) => {
   return (
-    <>
-      <h3>{title}</h3>
-    </>
+    <CalendarSection>
+      <HLower>{title}</HLower>
+      <CalendarFrameLarge
+        src="https://calendar.google.com/calendar/embed?showTitle=0&showPrint=0&showTabs=0&showCalendars=0&src=so2vs35e6ch68vl4map499fsm0@group.calendar.google.com&ctz=America%2FNew_York"
+        width="100%"
+        height="600"
+        scrolling="no"
+      ></CalendarFrameLarge>
+      <CalendarFrameSmall
+        src="https://calendar.google.com/calendar/embed?showTitle=0&mode=AGENDA&showPrint=0&showTabs=0&showCalendars=0&src=so2vs35e6ch68vl4map499fsm0@group.calendar.google.com&ctz=America%2FNew_York"
+        width="100%"
+        height="600"
+        scrolling="no"
+      ></CalendarFrameSmall>
+    </CalendarSection>
   )
 }
 
