1e6fed697d76e3e8a98b2fbea0fdb5e1415e7d2e
sreya-kata <sreya.kata@gmail.com>
2021-04-03 20:24:56 -0400

    graphql queries for title and subheading

--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -1,21 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import get from "lodash/get"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 import Header from "../components/header/header"
 
-const TeamPage = () => (
-  <Layout navbarStyle="gradient">
-    <SEO title="Team Page" />
-    <Header
-      title="Meet the team"
-      subheading="Learn a little about ViTAL key members"
-      imageBool={false}
-    />
-    <h1>Our Team</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const TeamPage = props => {
+  const teamPage = get(props, "data.contentfulTeamPage")
+
+  return (
+    <Layout navbarStyle="gradient">
+      <SEO title="Team Page" />
+      <Header
+        title={teamPage.title}
+        subheading={teamPage.subheadingForTitle}
+        imageBool={false}
+      />
+      <h1>Our Team</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default TeamPage
+
+export const pageQuery = graphql`
+  query teamQuery {
+    contentfulTeamPage {
+      title
+      subheadingForTitle
+    }
+  }
+`
