953dde177cecf58c59d8d03c4284dddd8d01028c
sreya-kata <sreya.kata@gmail.com>
2021-04-05 16:49:39 -0400

    platform layout for medium

--- src/components/what-we-do/platforms/platforms-styles.js
+++ src/components/what-we-do/platforms/platforms-styles.js
@@ -1,7 +1,60 @@
 import styled from "styled-components"
+import { H2 } from "../../../constants/typography"
+import { Button } from "../../../constants/buttons"
+import { size } from "../../../constants/devices"
+import { royalBlue } from "../../../constants/colors"
 
 const Title = styled.div`
   padding-left: 75px;
 `
 
-export { Title }
+const PlatformOne = styled.div`
+  display: flex;
+  width: 50vw;
+`
+
+const PlatformOneContent = styled.div`
+  width: 60%;
+`
+
+const LineContainerOne = styled.div`
+  width: 40%;
+  padding-top: 18px;
+  padding-right: 5px;
+`
+
+const LineOne = styled.div`
+  height: 3px;
+  background: ${royalBlue};
+`
+
+const PlatformTwo = styled.div``
+
+const LineContainerTwo = styled.div``
+
+const PlatformTitle = styled(H2)`
+  font-size: calc(
+    16px + (26 - 16) *
+      ((100vw - ${size.mobile}px) / (${size.desktopMax} - ${size.mobile}))
+  );
+`
+
+const PlatformButton = styled(Button)`
+  font-size: 12px;
+  padding: 16px 48px;
+`
+
+const Content = styled.div``
+
+export {
+  Title,
+  PlatformOne,
+  LineContainerOne,
+  PlatformOneContent,
+  LineOne,
+  PlatformTwo,
+  LineContainerTwo,
+  PlatformTitle,
+  PlatformButton,
+  Content,
+}
--- src/components/what-we-do/platforms/platforms.js
+++ src/components/what-we-do/platforms/platforms.js
@@ -1,28 +1,49 @@
 import React from "react"
 import { H2, P } from "../../../constants/typography"
-import { Title } from "./platforms-styles"
-import { Button } from "../../../constants/buttons"
+import {
+  Title,
+  PlatformOne,
+  LineContainerOne,
+  LineOne,
+  PlatformOneContent,
+  PlatformTwo,
+  LineContainerTwo,
+  PlatformTitle,
+  PlatformButton,
+  Content,
+} from "./platforms-styles"
 
 const Platforms = ({ title, platformOne, platformTwo }) => {
   return (
     <>
       <Title>
         <H2>{title}</H2>
-        <div>
-          <H2>{platformOne.platformTitle}</H2>
+      </Title>
+      <PlatformOne>
+        <LineContainerOne>
+          <LineOne />
+        </LineContainerOne>
+        <PlatformOneContent>
+          <PlatformTitle>{platformOne.platformTitle}</PlatformTitle>
           <P>{platformOne.blurb}</P>
           <a href={platformOne.ctaLink}>
-            <Button primary={true}>{platformOne.ctaTitle}</Button>
+            <PlatformButton primary={true}>
+              {platformOne.ctaTitle}
+            </PlatformButton>
           </a>
-        </div>
-        <div>
-          <H2>{platformTwo.platformTitle}</H2>
+        </PlatformOneContent>
+      </PlatformOne>
+      <PlatformTwo>
+        <Content>
+          <PlatformTitle>{platformTwo.platformTitle}</PlatformTitle>
           <P>{platformTwo.blurb}</P>
           <a href={platformTwo.ctaLink}>
-            <Button primary={true}>{platformTwo.ctaTitle}</Button>
+            <PlatformButton primary={true}>
+              {platformTwo.ctaTitle}
+            </PlatformButton>
           </a>
-        </div>
-      </Title>
+        </Content>
+      </PlatformTwo>
     </>
   )
 }
