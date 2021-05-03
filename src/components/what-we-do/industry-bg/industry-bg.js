90199adf30370b547ed638a8fe80ca5edca51d87
sreya-kata <sreya.kata@gmail.com>
2021-05-02 22:10:06 -0400

    derefrenced mobile indstry background image

--- src/components/what-we-do/industry-bg/industry-bg.js
+++ src/components/what-we-do/industry-bg/industry-bg.js
@@ -2,14 +2,14 @@ import React from "react"
 import { H2, P } from "../../../constants/typography"
 import { Container, TextContainer, Image } from "./industry-bg-styles"
 
-const IndustryBG = ({ title, blurb, mobileImage, imageAlt }) => {
+const IndustryBG = ({ title, blurb, image, imageAlt }) => {
   return (
     <Container>
       <TextContainer>
         <H2>{title}</H2>
         <P>{blurb.industryBackgroundBlurb}</P>
       </TextContainer>
-      <Image fluid={mobileImage.fluid} alt={imageAlt} />
+      <Image fluid={image.fluid} alt={imageAlt} />
     </Container>
   )
 }
