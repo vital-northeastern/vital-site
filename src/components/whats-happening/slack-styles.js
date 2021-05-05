c821413f403fc14d346a925458e6b7c94a57a24b
Ryanjso <ryanjsoderberg@gmail.com>
2021-05-05 16:38:00 -0400

    css and html for join slack block

---
+++ src/components/whats-happening/slack-styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const SlackContainer = styled.div`
+  height: 400px;
+  width: 75%;
+  background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.2) 0%,
+      rgba(255, 255, 255, 0) 100%
+    ),
+    linear-gradient(36.48deg, #a8d2ff 24.89%, #5269e1 111.91%);
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+export const SlackImg = styled.img`
+  margin-bottom: 0;
+`
+
+export const SlackContent = styled.div`
+  width: 55%;
+`
--- src/components/whats-happening/slack.js
+++ src/components/whats-happening/slack.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Img from "gatsby-image"
 
+import { SlackContainer, SlackContent, SlackImg } from "./slack-styles"
+import { Button } from "../../constants/buttons"
+import { P } from "../../constants/typography"
+
 const Slack = ({ title, link, logo }) => {
   return (
-    <>
-      <a href={link}>
-        <button type="button">{title}</button>
-      </a>
-      <Img className="featured" fluid={logo.fluid} alt={logo.description} />
-    </>
+    <SlackContainer>
+      <div>
+        <SlackImg src={logo.fluid.src} alt={logo.description}></SlackImg>
+      </div>
+      <SlackContent>
+        <P>
+          There is no better way to meet the e-board and other club members than
+          to join our Slack! You will get to meet new people through coffee
+          chats, stay updated on ViTAL events, and learn about news and
+          opportunities in the healthcare world!{" "}
+        </P>
+        <a href={link}>
+          <Button type="button" primary={true}>
+            {title}
+          </Button>
+        </a>
+      </SlackContent>
+    </SlackContainer>
   )
 }
 
