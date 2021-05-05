8d5ed48d09151df455332aa139944cf78be2fcd0
Ryanjso <ryanjsoderberg@gmail.com>
2021-05-05 14:07:14 -0400

    remove console logs

--- src/components/teampage/advisors/advisor-member.js
+++ src/components/teampage/advisors/advisor-member.js
@@ -1,23 +1,19 @@
 import React from "react"
 import { AdvisorCard, AdvisorImg } from "./advisor-member-styles"
 import { MemberName, ConnectLink, MemberInfo } from "../team-shared-styles"
-import Img from "gatsby-image"
 
 export default ({ name, photo, position, linkedinUrl }) => (
-  console.log(photo),
-  (
-    <AdvisorCard>
-      <AdvisorImg fluid={photo.fluid} alt={photo.description} />
-      <div>
-        <MemberName>{name}</MemberName>
+  <AdvisorCard>
+    <AdvisorImg fluid={photo.fluid} alt={photo.description} />
+    <div>
+      <MemberName>{name}</MemberName>
 
-        <MemberInfo>{position}</MemberInfo>
-        {linkedinUrl ? (
-          <ConnectLink href={linkedinUrl} target="_blank">
-            CONNECT
-          </ConnectLink>
-        ) : null}
-      </div>
-    </AdvisorCard>
-  )
+      <MemberInfo>{position}</MemberInfo>
+      {linkedinUrl ? (
+        <ConnectLink href={linkedinUrl} target="_blank">
+          CONNECT
+        </ConnectLink>
+      ) : null}
+    </div>
+  </AdvisorCard>
 )
